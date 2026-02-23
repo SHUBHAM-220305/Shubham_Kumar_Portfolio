eb8230f9fe20b710c04a0835dc2efaeef45af628
SHUBHAM-220305 <shubhamk.22032005@gmail.com>
2026-02-24 00:22:40 +0530

    Initial Commit 2

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -10,10 +10,13 @@ router.post("/", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,13 @@ app.get("/test-mail", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
